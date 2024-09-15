1b477dae7387449ee1a88a2178ce7a524e8c9f3a
San Kim <waterfog9580@gmail.com>
2024-09-15 16:00:39 +0900

    🩹 테스트 자동화

--- infrastructure/sms/src/test/kotlin/com/threedays/sms/auth/adapter/AuthCodeSmsSenderCoolSmsAdapterTest.kt
+++ infrastructure/sms/src/test/kotlin/com/threedays/sms/auth/adapter/AuthCodeSmsSenderCoolSmsAdapterTest.kt
@@ -2,21 +2,33 @@ package com.threedays.sms.auth.adapter
 
 import com.threedays.domain.auth.entity.AuthCode
 import com.threedays.domain.support.common.ClientOS
-import io.kotest.common.ExperimentalKotest
+import com.threedays.sms.support.SmsProperties
 import io.kotest.core.annotation.DisplayName
 import io.kotest.core.spec.style.DescribeSpec
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import net.nurigo.sdk.message.model.MessageType
+import net.nurigo.sdk.message.response.SingleMessageSentResponse
+import net.nurigo.sdk.message.service.DefaultMessageService
 import java.time.LocalDateTime
 
-@OptIn(ExperimentalKotest::class)
-@SpringBootTest
 @DisplayName("[SMS][인증] CoolSmsAdapter 인증코드 SMS 발송 테스트")
-class AuthCodeSmsSenderCoolSmsAdapterTest(
-    @Autowired private val authCodeSmsSenderCoolSmsAdapter: AuthCodeSmsSenderCoolSmsAdapter,
-) : DescribeSpec({
+class AuthCodeSmsSenderCoolSmsAdapterTest : DescribeSpec({
 
-    describe("인증코드 SMS 메시지 발송").config(enabled = false) {
+    val smsProperties = SmsProperties(
+        senderNumber = "01012345678", // 실제 번호로 변경
+        apiKey = "API",
+        apiSecret = "SECRET",
+        apiDomainUrl = "https://api.coolsms.co.kr"
+    )
+    val defaultMessageService: DefaultMessageService = mockk<DefaultMessageService>()
+    val authCodeSmsSenderCoolSmsAdapter = AuthCodeSmsSenderCoolSmsAdapter(
+        smsProperties = smsProperties,
+        defaultMessageService = defaultMessageService
+    )
+
+    describe("인증코드 SMS 메시지 발송") {
         it("phoneNumber 에 해당하는 번호로 인증코드를 발송한다.") {
             // arrange
             val authCode = AuthCode.create(
@@ -24,11 +36,23 @@ class AuthCodeSmsSenderCoolSmsAdapterTest(
                 phoneNumber = "01012345678", // 실제 번호로 변경
                 expireAt = LocalDateTime.now().plusMinutes(3)
             )
+            every { defaultMessageService.sendOne(any()) } returns SingleMessageSentResponse(
+                statusCode = "200",
+                statusMessage = "OK",
+                to = "01012345678", // 실제 번호로 변경
+                from = "01012345678", // 실제 번호로 변경
+                groupId = "GROUP_ID",
+                type = MessageType.SMS,
+                country = "82",
+                messageId = "MESSAGE_ID",
+                accountId = "ACCOUNT_ID",
+            )
 
             // act
             authCodeSmsSenderCoolSmsAdapter.send(authCode)
 
-            // assert 직접 확인
+            // assert
+            verify(exactly = 1) { defaultMessageService.sendOne(any()) }
         }
 
     }
