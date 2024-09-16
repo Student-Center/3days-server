8bd5e4709a475a1e274f9a44527bbd221ebce322
dojin <djyou128@gmail.com>
2024-09-16 19:51:55 +0900

    [Enhance] Add source set & change directory name (#10)
    * add source set & change directory name
    
    * remove unnecessary line
    
    * temp update gradle kts
    
    * add submoudle recursive
    
    * refactor library version
    
    * ♻️ refactor library version
    
    * ♻️ refactor library version
    
    ---------
    
    Co-authored-by: San Kim <waterfog9580@gmail.com>

--- bootstrap/api/build.gradle.kts
+++ bootstrap/api/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id(libs.plugins.openapi.generator.get().pluginId) version libs.plugins.openapi.generator.get().version.toString()
 }
@@ -7,30 +9,43 @@ dependencies {
     apply(plugin = rootProject.libs.plugins.openapi.generator.get().pluginId)
 
     implementation(project(":support:common"))
-
     implementation(project(":domain"))
     implementation(project(":application"))
     implementation(project(":infrastructure:persistence"))
     implementation(project(":infrastructure:sms"))
 
     implementation(libs.spring.boot.starter.web)
+    implementation(libs.springdoc.openapi.webmvc.ui)
+    implementation(libs.swagger.annotations)
+    implementation(libs.spring.boot.starter.validation)
 
     developmentOnly(libs.spring.boot.devtools)
     developmentOnly(libs.spring.boot.docker.compose)
 }
 
+val openApiGeneratePath = "${layout.buildDirectory.get()}/generated"
+
 openApiGenerate {
     generatorName.set("kotlin-spring")
     inputSpec.set("$rootDir/openapi/openapi.yaml")
-    outputDir.set("${layout.buildDirectory.get()}/generated")
-    apiPackage.set("com.sc.weave2.oas.api")
-    modelPackage.set("com.sc.weave2.oas.model")
-    configOptions.set(mapOf(
-        "interfaceOnly" to "true",
-        "useTags" to "true"
-    ))
+    outputDir.set(openApiGeneratePath)
+    apiPackage.set("com.threedays.oas.api")
+    modelPackage.set("com.threedays.oas.model")
+    configOptions.set(
+        mapOf(
+            "interfaceOnly" to "true",
+            "useTags" to "true",
+            "useSpringBoot3" to "true",
+        )
+    )
 }
 
-tasks.named("compileKotlin") {
+tasks.withType<KotlinCompile> {
     dependsOn("openApiGenerate")
+}
+
+sourceSets {
+    main {
+        kotlin.srcDir("$openApiGeneratePath/src/main/kotlin")
+    }
 }
