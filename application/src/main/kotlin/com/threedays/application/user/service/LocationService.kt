c26b955c7f6846f836d249330ab44a42d633f947
San Kim <waterfog9580@gmail.com>
2024-09-22 23:39:43 +0900

    🩹 트랜잭션 누락 추가 (#14)

--- application/src/main/kotlin/com/threedays/application/user/service/LocationService.kt
+++ application/src/main/kotlin/com/threedays/application/user/service/LocationService.kt
@@ -4,12 +4,14 @@ import com.threedays.application.user.port.inbound.QueryLocation
 import com.threedays.domain.user.entity.Location
 import com.threedays.domain.user.repository.LocationQueryRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class LocationService(
     private val locationQueryRepository: LocationQueryRepository,
 ) : QueryLocation {
 
+    @Transactional(readOnly = true)
     override fun findAll(): List<Location> {
         return locationQueryRepository.findAll()
     }
