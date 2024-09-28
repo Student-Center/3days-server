b26a856b7fb0038184650e863d96c73931a9b753
San Kim <waterfog9580@gmail.com>
2024-09-29 01:15:37 +0900

    ✨ 회원 가입 토큰 개발

--- domain/src/main/kotlin/com/threedays/domain/auth/exception/AuthException.kt
+++ domain/src/main/kotlin/com/threedays/domain/auth/exception/AuthException.kt
@@ -15,4 +15,12 @@ sealed class AuthException(
         override val message: String = "유효하지 않은 인증 코드입니다.",
     ) : AuthException(1002, message)
 
+    data class RegisterTokenExpiredException(
+        override val message: String = "회원 가입 토큰이 만료되었습니다.",
+    ) : AuthException(1003, message)
+
+    data class InvalidRegisterTokenException(
+        override val message: String = "유효하지 않은 회원 가입 토큰입니다.",
+    ) : AuthException(1004, message)
+
 }
---
+++ domain/src/test/kotlin/com/threedays/domain/auth/entity/RegisterTokenTest.kt
@@ -0,0 +1,96 @@
+package com.threedays.domain.auth.entity
+
+import com.threedays.domain.auth.exception.AuthException
+import com.threedays.support.common.security.jwt.JwtClaims
+import com.threedays.support.common.security.jwt.JwtTokenProvider
+import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.annotation.DisplayName
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import java.util.*
+
+@DisplayName("[도메인][Auth] - 회원 가입 토큰")
+class RegisterTokenTest : DescribeSpec({
+
+    describe("회원 가입 토큰 생성 - generate") {
+        it("회원 가입 토큰을 생성한다") {
+            // arrange
+            val secret = "secret"
+
+            // act
+            val registerToken = RegisterToken.generate(secret)
+
+            // assert
+            registerToken.value.isNotBlank() shouldBe true
+        }
+    }
+
+    describe("회원 가입 토큰 검증 - verify") {
+        context("회원 가입 토큰이 유효하면") {
+            it("예외가 발생하지 않는다") {
+                // arrange
+                val secret = "secret"
+                val registerToken = RegisterToken.generate(secret)
+
+                // act, assert
+                shouldNotThrowAny { RegisterToken.verify(registerToken.value, secret) }
+            }
+        }
+
+        context("회원 가입 토큰이 만료되었으면") {
+            it("AuthException.RegisterTokenExpiredException 예외가 발생한다") {
+                // arrange
+                val secret = "secret"
+                val token = JwtTokenProvider.createToken(
+                    JwtClaims {
+                        registeredClaims {
+                            sub = "register"
+                            exp = Date(System.currentTimeMillis() - 1000)
+                        }
+                    },
+                    secret
+                )
+
+                // act, assert
+                shouldThrow<AuthException.RegisterTokenExpiredException> {
+                    RegisterToken.verify(token, secret)
+                }
+            }
+        }
+
+        context("회원 가입 토큰이 JwtToken 형식이 아니면") {
+            it("AuthException.InvalidRegisterTokenException 예외가 발생한다") {
+                // arrange
+                val secret = "secret"
+                val invalidRegisterToken = "invalid"
+
+                // act, assert
+                shouldThrow<AuthException.InvalidRegisterTokenException> {
+                    RegisterToken.verify(invalidRegisterToken, secret)
+                }
+            }
+        }
+
+        context("회원 가입 토큰의 sub가 register가 아니면") {
+            it("AuthException.InvalidRegisterTokenException 예외가 발생한다") {
+                // arrange
+                val secret = "secret"
+                val invalidToken: String = JwtTokenProvider.createToken(
+                    JwtClaims {
+                        registeredClaims {
+                            sub = "invalid"
+                        }
+                    },
+                    secret
+                )
+
+                // act, assert
+                shouldThrow<AuthException.InvalidRegisterTokenException> {
+                    RegisterToken.verify(invalidToken, secret)
+                }
+            }
+        }
+    }
+
+})
