2f604632f957c606bdb6adbd7cb59425a4c6f340
San Kim <waterfog9580@gmail.com>
2024-10-03 12:45:16 +0900

    🩹 리뷰 반영

--- domain/src/main/kotlin/com/threedays/domain/auth/exception/AuthException.kt
+++ domain/src/main/kotlin/com/threedays/domain/auth/exception/AuthException.kt
@@ -31,28 +31,20 @@ sealed class AuthException(
         override val message: String = "이미 가입된 사용자입니다.",
     ) : AuthException(1005, message)
 
-    /**
-     * 가입 된 사용자에 대해 인증 코드 인증을 요청할때
-     * 가입되지 않은 사용자라면 발생하는 예외
-     */
-    data class UserNotExistsException(
-        override val message: String = "가입되지 않은 사용자입니다.",
-    ) : AuthException(1006, message)
-
     data class AccessTokenExpiredException(
         override val message: String = "Access Token이 만료되었습니다.",
-    ) : AuthException(1007, message)
+    ) : AuthException(1006, message)
 
     data class InvalidAccessTokenException(
         override val message: String = "유효하지 않은 Access Token입니다.",
-    ) : AuthException(1008, message)
+    ) : AuthException(1007, message)
 
     data class RefreshTokenExpiredException(
         override val message: String = "Refresh Token이 만료되었습니다.",
-    ) : AuthException(1009, message)
+    ) : AuthException(1008, message)
 
     data class InvalidRefreshTokenException(
         override val message: String = "유효하지 않은 Refresh Token입니다.",
-    ) : AuthException(1010, message)
+    ) : AuthException(1009, message)
 
 }
