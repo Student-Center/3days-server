0ddcc72a06e11c546a3f7ceb355bda72110a328e
San Kim <waterfog9580@gmail.com>
2024-10-09 00:26:12 +0900

    [WEAV-34] ✨ 직장 검색 API (#25)
    * ✨ 직장 검색 API
    
    * 🩹 리뷰반영

--- infrastructure/persistence/src/main/kotlin/com/threedays/persistence/user/adapter/CompanyQueryPersistenceAdapter.kt
+++ infrastructure/persistence/src/main/kotlin/com/threedays/persistence/user/adapter/CompanyQueryPersistenceAdapter.kt
@@ -1,16 +1,51 @@
 package com.threedays.persistence.user.adapter
 
+import com.linecorp.kotlinjdsl.dsl.jpql.jpql
+import com.linecorp.kotlinjdsl.querymodel.jpql.select.SelectQuery
+import com.linecorp.kotlinjdsl.render.RenderContext
+import com.linecorp.kotlinjdsl.support.spring.data.jpa.extension.createQuery
 import com.threedays.domain.user.entity.Company
 import com.threedays.domain.user.repository.CompanyQueryRepository
+import com.threedays.persistence.user.entity.CompanyJpaEntity
 import com.threedays.persistence.user.repository.CompanyJpaRepository
+import jakarta.persistence.EntityManager
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
 @Component
 @Transactional(readOnly = true)
 class CompanyQueryPersistenceAdapter(
-    private val companyJpaRepository: CompanyJpaRepository
+    private val companyJpaRepository: CompanyJpaRepository,
+    private val entityManager: EntityManager,
+    private val jdslRenderContext: RenderContext,
 ) : CompanyQueryRepository {
+
+    override fun searchCompanies(
+        name: String,
+        next: Company.Id?,
+        limit: Int
+    ): Pair<List<Company>, Company.Id?> {
+        val query: SelectQuery<CompanyJpaEntity> = jpql {
+            select(
+                entity(CompanyJpaEntity::class)
+            ).from(
+                entity(CompanyJpaEntity::class)
+            ).whereAnd(
+                path(CompanyJpaEntity::name).like("%$name%"),
+                next?.let { path(CompanyJpaEntity::id).gt(it.value) }
+            )
+        }
+
+        val result: List<Company> = entityManager
+            .createQuery(query, jdslRenderContext)
+            .apply { setMaxResults(limit + 1) }
+            .resultList
+            .map { it.toDomainEntity() }
+
+        val nextId: Company.Id? = result.getOrNull(limit)?.id ?: next
+
+        return result.take(limit) to nextId
+    }
 
     override fun find(id: Company.Id): Company? {
         return companyJpaRepository
--- infrastructure/persistence/src/main/kotlin/com/threedays/persistence/user/repository/CompanyJpaRepository.kt
+++ infrastructure/persistence/src/main/kotlin/com/threedays/persistence/user/repository/CompanyJpaRepository.kt
@@ -1,9 +1,10 @@
 package com.threedays.persistence.user.repository
 
+import com.linecorp.kotlinjdsl.support.spring.data.jpa.repository.KotlinJdslJpqlExecutor
 import com.threedays.persistence.user.entity.CompanyJpaEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
-interface CompanyJpaRepository : JpaRepository<CompanyJpaEntity, UUID>
+interface CompanyJpaRepository : JpaRepository<CompanyJpaEntity, UUID>, KotlinJdslJpqlExecutor
