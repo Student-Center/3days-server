f71159d5b09cc0ff2a644ff3816323aea1696a1c
San Kim <waterfog9580@gmail.com>
2024-10-10 23:09:18 +0900

    ✨ Location API 분리 (region, sub_region)
    Replaced searchLocations with more specific methods: getLocationRegions and getLocationsByRegion. These changes improve clarity and specificity in location-related queries by focusing on distinct operations. Updated corresponding repository and adapter classes to support these new methods.

--- bootstrap/api/src/main/kotlin/com/threedays/bootstrap/api/user/LocationsController.kt
+++ bootstrap/api/src/main/kotlin/com/threedays/bootstrap/api/user/LocationsController.kt
@@ -3,42 +3,29 @@ package com.threedays.bootstrap.api.user
 import com.threedays.domain.user.repository.LocationQueryRepository
 import com.threedays.oas.api.LocationsApi
 import com.threedays.oas.model.Location
-import com.threedays.oas.model.SearchLocationsResponse
-import com.threedays.support.common.base.domain.UUIDTypeId
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
-import java.util.*
 
 @RestController
 class LocationsController(
     private val locationQueryRepository: LocationQueryRepository,
 ) : LocationsApi {
 
-    override fun searchLocations(
-        name: String,
-        next: UUID?,
-        limit: Int
-    ): ResponseEntity<SearchLocationsResponse> {
-        val (resultLocation, resultNext) = locationQueryRepository.searchLocations(
-            name = name,
-            next = next?.let { UUIDTypeId.from(it) },
-            limit = limit
-        )
+    override fun getLocationRegions(): ResponseEntity<List<String>> {
+        return ResponseEntity.ok(locationQueryRepository.getLocationRegions())
+    }
 
-        val locationResponse: List<Location> = resultLocation.map {
-            Location(
-                id = it.id.value,
-                region = it.region.value,
-                subRegion = it.subRegion.value,
-            )
-        }
-
-        return ResponseEntity.ok(
-            SearchLocationsResponse(
-                locations = locationResponse,
-                next = resultNext?.value
-            )
-        )
+    override fun getLocationsByRegion(regionName: String): ResponseEntity<List<Location>> {
+        return locationQueryRepository
+            .getLocationsByRegion(regionName)
+            .map {
+                Location(
+                    id = it.id.value,
+                    region = it.region.value,
+                    subRegion = it.subRegion.value,
+                )
+            }
+            .let { ResponseEntity.ok(it) }
     }
 
 }
