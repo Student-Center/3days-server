0776504555f7f1970e8f2bc867e5e37811880bfc
San Kim <waterfog9580@gmail.com>
2024-12-20 00:03:35 +0900

    fix

--- domain/src/main/kotlin/com/threedays/domain/chat/entity/ChatMessage.kt
+++ domain/src/main/kotlin/com/threedays/domain/chat/entity/ChatMessage.kt
@@ -21,7 +21,6 @@ data class ChatMessage(
 
     enum class Status {
         SENT,
-        DELIVERED,
         READ,
         FAILED,
     }
@@ -33,23 +32,22 @@ data class ChatMessage(
         UNKNOWN_ERROR
     }
 
-    fun markAsDelivered(): ChatMessage =
-        this.copy(
-            status = Status.DELIVERED,
-            updatedAt = LocalDateTime.now()
-        )
-
-    fun markAsRead(): ChatMessage =
-        this.copy(
+    fun markAsRead(): ChatMessage {
+        require(status == Status.SENT) { "메시지가 전송된 상태에서만 읽음 처리가 가능합니다" }
+        return this.copy(
             status = Status.READ,
             updatedAt = LocalDateTime.now()
         )
+    }
 
-    fun markAsFailed(reason: FailureReason) =
+
+    fun markAsFailed(reason: FailureReason) {
+        require(status == Status.SENT) { "전송된 상태의 메시지만 실패 처리가 가능합니다" }
         this.copy(
             status = Status.FAILED,
             updatedAt = LocalDateTime.now(),
             failureReason = reason
         )
+    }
 
 }
