b183b179c5709ab4a15b755ed7702d413d38b2ed
dojin <djyou128@gmail.com>
2025-02-24 01:46:32 +0900

    add functional interface annotation

--- infrastructure/rest/src/main/kotlin/com/threedays/rest/client/DiscordClient.kt
+++ infrastructure/rest/src/main/kotlin/com/threedays/rest/client/DiscordClient.kt
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import java.net.URI
 
+@FunctionalInterface
 @FeignClient(
     name = "discord",
     url = "dyanamic-discord-client-url"
