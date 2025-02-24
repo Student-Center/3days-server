64d048026ea576e68bc223d1213d6ab767687e5d
dojin <djyou128@gmail.com>
2025-02-25 08:20:58 +0900

    fix(config): Add discord properties profiles (#47)

--- infrastructure/rest/src/main/kotlin/com/threedays/rest/support/properties/DiscordProperties.kt
+++ infrastructure/rest/src/main/kotlin/com/threedays/rest/support/properties/DiscordProperties.kt
@@ -1,7 +1,9 @@
 package com.threedays.rest.support.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Profile
 
+@Profile("prod", "dev", "local")
 @ConfigurationProperties(prefix = "discord")
 @Suppress("unused")
 data class DiscordProperties(
