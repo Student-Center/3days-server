cab82837e9142cd596ca5cfcec7549309be60ad2
dojin <djyou128@gmail.com>
2025-04-08 00:27:33 +0900

    Connection 관련 도메인 테스트 코드 작성

--- domain/src/test/kotlin/com/threedays/domain/connection/entity/ConnectionAttemptTest.kt
+++ domain/src/test/kotlin/com/threedays/domain/connection/entity/ConnectionAttemptTest.kt
@@ -1,46 +1,91 @@
-package com.threedays.domain.user.entity
+package com.threedays.domain.connection.entity
 
-import com.threedays.domain.user.exception.UserException
-import io.kotest.core.spec.DisplayName
+import com.navercorp.fixturemonkey.FixtureMonkey
+import com.navercorp.fixturemonkey.kotlin.giveMeBuilder
+import com.navercorp.fixturemonkey.kotlin.giveMeOne
+import com.navercorp.fixturemonkey.kotlin.introspector.PrimaryConstructorArbitraryIntrospector
+import com.navercorp.fixturemonkey.kotlin.set
+import com.threedays.domain.user.entity.User
+import com.threedays.support.common.base.domain.UUIDTypeId
+import io.kotest.core.annotation.DisplayName
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
-import java.net.URI
-import java.net.URL
+import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.types.shouldBeInstanceOf
+import java.util.UUID
 
-@DisplayName("[도메인][연결] 연결 시도")
+@DisplayName("[도메인][연결] - 연결 시도")
 class ConnectionAttemptTest : DescribeSpec({
 
-    describe("프로필 이미지 생성") {
-        UserProfileImage.Extension.entries.forEach { extension ->
-            it("확장자가 ${extension.value}인 프로필 이미지를 생성한다") {
-                // arrange
-                val getProfileImageUrlAction: (UserProfileImage.Id, UserProfileImage.Extension) -> URL =
-                    { _, _ -> URI("https://example.com").toURL() }
+    val fixtureMonkey: FixtureMonkey = FixtureMonkey.builder()
+        .objectIntrospector(PrimaryConstructorArbitraryIntrospector.INSTANCE)
+        .build()
 
-                // act
-                val userProfileImage: UserProfileImage =
-                    UserProfileImage.create(extension, getProfileImageUrlAction)
+    describe("연결 시도 생성") {
+        it("새로운 연결 시도를 생성한다") {
+            // arrange
+            val userId = User.Id(UUID.randomUUID())
 
-                // assert
-                userProfileImage.extension shouldBe extension
-            }
-        }
+            // act
+            val connectionAttempt = ConnectionAttempt.create(userId)
 
-        context("URL 생성작업에서 예외가 발생하는 경우") {
-            it("UserException.ProfileImageUploadFailedException 예외를 던진다") {
-                // arrange
-                val getProfileImageUrlAction: (UserProfileImage.Id, UserProfileImage.Extension) -> URL =
-                    { _, _ -> throw Exception() }
-
-                // act & assert
-                runCatching {
-                    UserProfileImage.create(
-                        UserProfileImage.Extension.PNG,
-                        getProfileImageUrlAction
-                    )
-                }.onFailure { it shouldBe UserException.ProfileImageUploadFailedException() }
-            }
+            // assert
+            connectionAttempt.id shouldNotBe null
+            connectionAttempt.userId shouldBe userId
+            connectionAttempt.status shouldBe ConnectionAttempt.Status.CONNECTING
+            connectionAttempt.connection shouldBe null
         }
     }
 
+    describe("연결 성공") {
+        it("연결 성공 상태로 변경한다") {
+            // arrange
+            val connectionAttempt = fixtureMonkey.giveMeBuilder<ConnectionAttempt>()
+                .set(ConnectionAttempt::status, ConnectionAttempt.Status.CONNECTING)
+                .sample()
+            val connection = fixtureMonkey.giveMeOne<Connection>()
+
+            // act
+            val connectedAttempt = connectionAttempt.connect(connection)
+
+            // assert
+            connectedAttempt.status shouldBe ConnectionAttempt.Status.CONNECTED
+            connectedAttempt.connection shouldBe connection
+        }
+    }
+
+    describe("연결 실패") {
+        it("연결 실패 상태로 변경한다") {
+            // arrange
+            val connectionAttempt = fixtureMonkey.giveMeBuilder<ConnectionAttempt>()
+                .set(ConnectionAttempt::status, ConnectionAttempt.Status.CONNECTING)
+                .sample()
+
+            // act
+            val failedAttempt = connectionAttempt.fail()
+
+            // assert
+            failedAttempt.status shouldBe ConnectionAttempt.Status.FAILED
+            failedAttempt.connection shouldBe connectionAttempt.connection
+        }
+    }
+
+    describe("Status.from") {
+        it("문자열로부터 Status 객체를 생성한다") {
+            ConnectionAttempt.Status.entries.forEach { status ->
+                ConnectionAttempt.Status.from(status.toString()) shouldBe status
+            }
+        }
+
+        it("알 수 없는 문자열로부터 UNKNOWN Status 객체를 생성한다") {
+            ConnectionAttempt.Status.from("INVALID_STATUS") shouldBe ConnectionAttempt.Status.UNKNOWN
+        }
+    }
+
+    describe("Id") {
+        it("UUIDTypeId를 상속받는다") {
+            val id = ConnectionAttempt.Id(UUID.randomUUID())
+            id.shouldBeInstanceOf<UUIDTypeId>()
+        }
+    }
 })
--- domain/src/test/kotlin/com/threedays/domain/connection/entity/ConnectionTest.kt
+++ domain/src/test/kotlin/com/threedays/domain/connection/entity/ConnectionTest.kt
@@ -1,344 +1,112 @@
 package com.threedays.domain.connection.entity
 
-import com.navercorp.fixturemonkey.FixtureMonkey
-import com.navercorp.fixturemonkey.kotlin.giveMeBuilder
-import com.navercorp.fixturemonkey.kotlin.introspector.PrimaryConstructorArbitraryIntrospector
-import com.navercorp.fixturemonkey.kotlin.set
-import com.threedays.domain.auth.vo.PhoneNumber
-import com.threedays.domain.user.reposiotry.CompanyQueryRepositorySpy
-import com.threedays.domain.user.reposiotry.LocationQueryRepositorySpy
-import com.threedays.domain.user.vo.BirthYearRange
-import com.threedays.domain.user.vo.Gender
-import com.threedays.domain.user.vo.JobOccupation
-import io.kotest.assertions.throwables.shouldThrow
-import io.kotest.core.annotation.DisplayName
+import com.threedays.domain.user.entity.User
+import com.threedays.support.common.base.domain.UUIDTypeId
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
-import java.net.URL
-import java.time.Year
+import com.navercorp.fixturemonkey.FixtureMonkey
+import com.navercorp.fixturemonkey.kotlin.KotlinPlugin
+import com.navercorp.fixturemonkey.kotlin.giveMeOne
+import io.kotest.core.annotation.DisplayName
+import io.kotest.matchers.comparables.shouldBeGreaterThan
+import io.kotest.matchers.comparables.shouldBeLessThan
+import io.kotest.matchers.types.shouldBeInstanceOf
+import java.time.LocalDateTime
+import java.util.UUID
 
-@DisplayName("[도메인][연결] 연결")
+@DisplayName("[도메인][연결] - 연결")
 class ConnectionTest : DescribeSpec({
 
     val fixtureMonkey: FixtureMonkey = FixtureMonkey.builder()
-        .objectIntrospector(PrimaryConstructorArbitraryIntrospector.INSTANCE)
+        .plugin(KotlinPlugin())
         .build()
 
-    val locationQueryRepository = LocationQueryRepositorySpy()
-    val companyQueryRepository = CompanyQueryRepositorySpy()
-
-    afterTest {
-        locationQueryRepository.clear()
-        companyQueryRepository.clear()
-    }
-
-    describe("유저 생성") {
-        it("새로운 유저를 생성한다") {
+    describe("Connection 생성") {
+        it("두 유저를 매칭하여 Connection을 생성한다") {
             // arrange
-            val name: User.Name = fixtureMonkey.giveMeBuilder<User.Name>().sample()
-            val phoneNumber: PhoneNumber = PhoneNumber("01012345678")
-            val userGender: Gender = fixtureMonkey.giveMeBuilder<Gender>().sample()
-            val userBirthYear: Year = fixtureMonkey.giveMeBuilder<Year>().sample()
-            val userCompany: Company = fixtureMonkey.giveMeBuilder<Company>().sample()
-            val userJobOccupation: JobOccupation =
-                fixtureMonkey.giveMeBuilder<JobOccupation>().sample()
-            val userLocations: List<Location> =
-                fixtureMonkey.giveMeBuilder<Location>().sampleList(3)
-            val partnerBirthYearRange: BirthYearRange =
-                fixtureMonkey.giveMeBuilder<BirthYearRange>().sample()
-            val partnerJobOccupations: List<JobOccupation> =
-                fixtureMonkey.giveMeBuilder<JobOccupation>().sampleList(10)
-            val partnerPreferDistance: UserDesiredPartner.PreferDistance =
-                fixtureMonkey.giveMeBuilder<UserDesiredPartner.PreferDistance>().sample()
+            val user1Id = User.Id(UUID.randomUUID())
+            val user2Id = User.Id(UUID.randomUUID())
+            val beforeConnectAt = LocalDateTime.now()
 
             // act
-            val user = User.create(
-                name = name,
-                phoneNumber = phoneNumber,
-                userGender = userGender,
-                userBirthYear = userBirthYear,
-                userCompany = userCompany,
-                userJobOccupation = userJobOccupation,
-                userLocations = userLocations,
-                partnerBirthYearRange = partnerBirthYearRange,
-                partnerJobOccupations = partnerJobOccupations,
-                partnerPreferDistance = partnerPreferDistance,
-                allowSameCompany = false,
-            )
+            val connection = Connection.match(user1Id, user2Id)
 
             // assert
-            user.id shouldNotBe null
-            user.phoneNumber shouldBe phoneNumber
-            user.name shouldBe name
-            user.profile.gender shouldBe userGender
-            user.profile.birthYear shouldBe userBirthYear
-            user.profile.company shouldBe userCompany
-            user.profile.jobOccupation shouldBe userJobOccupation
-            user.profile.locations shouldBe userLocations
-            user.desiredPartner.birthYearRange shouldBe partnerBirthYearRange
-            user.desiredPartner.jobOccupations shouldBe partnerJobOccupations
-            user.desiredPartner.preferDistance shouldBe partnerPreferDistance
-        }
-
-        context("동일 회사 허용 여부를 선택한 경우") {
-            it("회사를 선택하지 않으면 예외가 발생한다") {
-                // arrange
-                val name: User.Name = fixtureMonkey.giveMeBuilder<User.Name>().sample()
-                val phoneNumber: PhoneNumber = PhoneNumber("01012345678")
-                val userGender: Gender = fixtureMonkey.giveMeBuilder<Gender>().sample()
-                val userBirthYear: Year = fixtureMonkey.giveMeBuilder<Year>().sample()
-                val userJobOccupation: JobOccupation =
-                    fixtureMonkey.giveMeBuilder<JobOccupation>().sample()
-                val userLocations: List<Location> =
-                    fixtureMonkey.giveMeBuilder<Location>().sampleList(3)
-                val partnerBirthYearRange: BirthYearRange =
-                    fixtureMonkey.giveMeBuilder<BirthYearRange>().sample()
-                val partnerJobOccupations: List<JobOccupation> =
-                    fixtureMonkey.giveMeBuilder<JobOccupation>().sampleList(10)
-                val partnerPreferDistance: UserDesiredPartner.PreferDistance =
-                    fixtureMonkey.giveMeBuilder<UserDesiredPartner.PreferDistance>().sample()
-
-                val allowSameCompany: Boolean = true
-                val userCompany: Company? = null
-
-                // act & assert
-                shouldThrow<IllegalArgumentException> {
-                    User.create(
-                        name = name,
-                        phoneNumber = phoneNumber,
-                        userGender = userGender,
-                        userBirthYear = userBirthYear,
-                        userCompany = userCompany,
-                        userJobOccupation = userJobOccupation,
-                        userLocations = userLocations,
-                        partnerBirthYearRange = partnerBirthYearRange,
-                        partnerJobOccupations = partnerJobOccupations,
-                        partnerPreferDistance = partnerPreferDistance,
-                        allowSameCompany = allowSameCompany,
-                    )
-                }
-            }
-        }
-
-        context("동일 회사 허용 여부를 선택하지 않은 경우") {
-            it("회사를 선택하면 예외가 발생한다") {
-                // arrange
-                val phoneNumber: PhoneNumber = PhoneNumber("01012345678")
-                val userGender: Gender = fixtureMonkey.giveMeBuilder<Gender>().sample()
-                val userBirthYear: Year = fixtureMonkey.giveMeBuilder<Year>().sample()
-                val userJobOccupation: JobOccupation =
-                    fixtureMonkey.giveMeBuilder<JobOccupation>().sample()
-                val userLocations: List<Location> =
-                    fixtureMonkey.giveMeBuilder<Location>().sampleList(3)
-                val partnerBirthYearRange: BirthYearRange =
-                    fixtureMonkey.giveMeBuilder<BirthYearRange>().sample()
-                val partnerJobOccupations: List<JobOccupation> =
-                    fixtureMonkey.giveMeBuilder<JobOccupation>().sampleList(10)
-                val partnerPreferDistance: UserDesiredPartner.PreferDistance =
-                    fixtureMonkey.giveMeBuilder<UserDesiredPartner.PreferDistance>().sample()
-
-                val allowSameCompany: Boolean = true
-                val userCompany: Company? = null
-
-                // act & assert
-                shouldThrow<IllegalArgumentException> {
-                    User.create(
-                        name = User.Name("홍길동"),
-                        phoneNumber = phoneNumber,
-                        userGender = userGender,
-                        userBirthYear = userBirthYear,
-                        userCompany = userCompany,
-                        userJobOccupation = userJobOccupation,
-                        userLocations = userLocations,
-                        partnerBirthYearRange = partnerBirthYearRange,
-                        partnerJobOccupations = partnerJobOccupations,
-                        partnerPreferDistance = partnerPreferDistance,
-                        allowSameCompany = allowSameCompany,
-                    )
-                }
-            }
-        }
-
-        context("이름이 공백일 경우") {
-            it("예외가 발생한다") {
-                // arrange
-                val name = ""
-
-                // act & assert
-                shouldThrow<IllegalArgumentException> {
-                    User.Name(name)
-                }
-            }
+            connection.id shouldNotBe null
+            connection.connectedAt shouldBeGreaterThan  beforeConnectAt
+            connection.cancellation shouldBe null
+            connection.isParticipant(user1Id) shouldBe true
+            connection.isParticipant(user2Id) shouldBe true
         }
     }
 
-    describe("putProfileWidget - 프로필 위젯 추가") {
-        it("프로필 위젯을 추가한다") {
+    describe("cancel") {
+        it("Connection을 취소하고 취소 정보를 설정한다") {
             // arrange
-            val profileWidget: ProfileWidget = fixtureMonkey
-                .giveMeBuilder<ProfileWidget>()
-                .sample()
-
-            val userDesiredPartner: UserDesiredPartner = fixtureMonkey
-                .giveMeBuilder<UserDesiredPartner>()
-                .set(UserDesiredPartner::allowSameCompany, null)
-                .sample()
-
-            val userProfileImageUrl: URL = URL("http://example.com")
-            val userProfileImage = fixtureMonkey
-                .giveMeBuilder<UserProfileImage>()
-                .set(UserProfileImage::url, userProfileImageUrl)
-                .sampleList(1)
-
-            val user: User = fixtureMonkey
-                .giveMeBuilder<User>()
-                .set(User::name, User.Name("홍길동"))
-                .set(User::phoneNumber, PhoneNumber("01012345678"))
-                .set(User::profileImages, userProfileImage)
-                .set(User::desiredPartner, userDesiredPartner)
-                .sample()
+            val connection = fixtureMonkey.giveMeOne<Connection>()
+            val userId = User.Id(UUID.randomUUID())
+            val reason = ConnectionCancellation.Reason.ETC
+            val detail = "개인적인 사유로 취소합니다."
 
             // act
-            val result: User = user.putProfileWidget(profileWidget)
+            val canceledConnection = connection.cancel(userId, reason, detail)
 
             // assert
-            result.profile.profileWidgets.find { it == profileWidget } shouldBe profileWidget
+            canceledConnection.cancellation shouldNotBe null
+            canceledConnection.cancellation?.userId shouldBe userId
+            canceledConnection.cancellation?.reason shouldBe reason
+            canceledConnection.cancellation?.detail shouldBe detail
         }
 
-        ProfileWidget.Type.entries.forEach { widgetType ->
-        context("동일한 타입($widgetType)의 위젯이 이미 있는 경우") {
-                it("$widgetType 타입의 위젯 내용을 수정한다") {
-                    // arrange
-                    val profileWidget: ProfileWidget = fixtureMonkey
-                        .giveMeBuilder<ProfileWidget>()
-                        .set(ProfileWidget::type, widgetType)
-                        .sample()
-
-                    val userDesiredPartner: UserDesiredPartner = fixtureMonkey
-                        .giveMeBuilder<UserDesiredPartner>()
-                        .set(UserDesiredPartner::allowSameCompany, null)
-                        .sample()
-
-                    val userProfileImageUrl: URL = URL("http://example.com")
-                    val userProfileImage = fixtureMonkey
-                        .giveMeBuilder<UserProfileImage>()
-                        .set(UserProfileImage::url, userProfileImageUrl)
-                        .sampleList(1)
-
-                    val user: User = fixtureMonkey
-                        .giveMeBuilder<User>()
-                        .set(User::name, User.Name("홍길동"))
-                        .set(User::phoneNumber, PhoneNumber("01012345678"))
-                        .set(User::desiredPartner, userDesiredPartner)
-                        .set(User::profileImages, userProfileImage)
-                        .sample()
-                        .putProfileWidget(profileWidget)
-
-                    val updatedProfileWidget: ProfileWidget = fixtureMonkey
-                        .giveMeBuilder<ProfileWidget>()
-                        .set(ProfileWidget::type, widgetType)
-                        .sample()
-
-                    // act
-                    val result: User = user.putProfileWidget(updatedProfileWidget)
-
-                    // assert
-                    result.profile.profileWidgets.find { it == updatedProfileWidget } shouldBe updatedProfileWidget
-                }
-            }
-        }
-    }
-
-    describe("updateUserInfo - 유저 정보 수정") {
-        it("유저 정보를 수정한다") {
+        it("취소 상세 정보가 null일 때도 Connection을 취소한다") {
             // arrange
-            val userDesiredPartner: UserDesiredPartner = fixtureMonkey
-                .giveMeBuilder<UserDesiredPartner>()
-                .set(UserDesiredPartner::allowSameCompany, null)
-                .sample()
-
-            val userProfileImageUrl: URL = URL("http://example.com")
-            val userProfileImage = fixtureMonkey
-                .giveMeBuilder<UserProfileImage>()
-                .set(UserProfileImage::url, userProfileImageUrl)
-                .sampleList(1)
-
-            val user: User = fixtureMonkey
-                .giveMeBuilder<User>()
-                .set(User::name, User.Name("홍길동"))
-                .set(User::phoneNumber, PhoneNumber("01012345678"))
-                .set(User::profileImages, userProfileImage)
-                .set(User::desiredPartner, userDesiredPartner)
-                .sample()
-
-            val name: User.Name = fixtureMonkey.giveMeBuilder<User.Name>().sample()
-            val jobOccupation: JobOccupation = fixtureMonkey.giveMeBuilder<JobOccupation>().sample()
-            val locations = fixtureMonkey.giveMeBuilder<Location>().sampleList(3)
-            val company = fixtureMonkey.giveMeBuilder<Company>().sample()
-            val allowSameCompany: Boolean = fixtureMonkey.giveMeBuilder<Boolean>().sample()
-
-            companyQueryRepository.save(company)
-            locations.forEach { locationQueryRepository.save(it) }
+            val connection = fixtureMonkey.giveMeOne<Connection>()
+            val userId = User.Id(UUID.randomUUID())
+            val reason = ConnectionCancellation.Reason.ETC
 
             // act
-            val result: User = user.updateUserInfo(
-                name = name,
-                jobOccupation = jobOccupation,
-                locationIds = locations.map { it.id },
-                locationQueryRepository = locationQueryRepository,
-                companyId = company.id,
-                companyQueryRepository = companyQueryRepository,
-                allowSameCompany = allowSameCompany,
-            )
+            val canceledConnection = connection.cancel(userId, reason)
 
             // assert
-            result.name shouldBe name
-            result.profile.jobOccupation shouldBe jobOccupation
-            result.profile.locations shouldBe locations
-            result.profile.company shouldBe company
+            canceledConnection.cancellation shouldNotBe null
+            canceledConnection.cancellation?.userId shouldBe userId
+            canceledConnection.cancellation?.reason shouldBe reason
+            canceledConnection.cancellation?.detail shouldBe null
         }
     }
 
-    describe("updateDesiredPartner - 이상형 프로필 수정") {
-        it("이상형 프로필을 수정한다") {
+    describe("isParticipant") {
+        it("userId가 participant1의 id와 같으면 true를 반환한다") {
             // arrange
-            val userDesiredPartner: UserDesiredPartner = fixtureMonkey
-                .giveMeBuilder<UserDesiredPartner>()
-                .set(UserDesiredPartner::allowSameCompany, null)
-                .sample()
+            val connection = fixtureMonkey.giveMeOne<Connection>()
 
-            val userProfileImageUrl: URL = URL("http://example.com")
-            val userProfileImage = fixtureMonkey
-                .giveMeBuilder<UserProfileImage>()
-                .set(UserProfileImage::url, userProfileImageUrl)
-                .sampleList(1)
+            // act & assert
+            connection.isParticipant(connection.participant1.id) shouldBe true
+        }
 
-            val user: User = fixtureMonkey
-                .giveMeBuilder<User>()
-                .set(User::name, User.Name("홍길동"))
-                .set(User::profileImages, userProfileImage)
-                .set(User::phoneNumber, PhoneNumber("01012345678"))
-                .set(User::desiredPartner, userDesiredPartner)
-                .sample()
+        it("userId가 participant2의 id와 같으면 true를 반환한다") {
+            // arrange
+            val connection = fixtureMonkey.giveMeOne<Connection>()
 
-            val birthYearRange: BirthYearRange = fixtureMonkey.giveMeBuilder<BirthYearRange>().sample()
-            val jobOccupations: List<JobOccupation> = fixtureMonkey.giveMeBuilder<JobOccupation>().sampleList(10)
-            val preferDistance: UserDesiredPartner.PreferDistance =
-                fixtureMonkey.giveMeBuilder<UserDesiredPartner.PreferDistance>().sample()
+            // act & assert
+            connection.isParticipant(connection.participant2.id) shouldBe true
+        }
 
-            // act
-            val result: User = user.updateDesiredPartner(
-                birthYearRange = birthYearRange,
-                jobOccupations = jobOccupations,
-                preferDistance = preferDistance,
-            )
+        it("userId가 participant1, participant2의 id와 모두 다르면 false를 반환한다") {
+            // arrange
+            val connection = fixtureMonkey.giveMeOne<Connection>()
+            val otherUserId = User.Id(UUID.randomUUID())
 
-            // assert
-            result.desiredPartner.birthYearRange shouldBe birthYearRange
-            result.desiredPartner.jobOccupations shouldBe jobOccupations
-            result.desiredPartner.preferDistance shouldBe preferDistance
+            // act & assert
+            connection.isParticipant(otherUserId) shouldBe false
         }
     }
 
+    describe("Id") {
+        it("UUIDTypeId를 상속받는다") {
+            val id = Connection.Id(UUID.randomUUID())
+            id.shouldBeInstanceOf<UUIDTypeId>()
+        }
+    }
 })
